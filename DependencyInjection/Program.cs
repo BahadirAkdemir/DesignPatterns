d1081d57ce0ee14e5e0a99706567c0b90a776263
BahadirAkdemir <akdemir_bahadir@hotmail.com>
2021-07-20 10:08:37 +0300

    Adding new design patterns

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            kernel.Bind<IProductDal>().To<NHProductDal>().InSingletonScope();
+            ProductManager productManager = new ProductManager(kernel.Get<IProductDal>());
+            productManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IProductDal
+    {
+        void Save();
+
+    }
+
+    class EFProductDal:IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Save eith EF");
+        }
+    }
+
+    class NHProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Save eith NH");
+        }
+    }
+
+    class ProductManager
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal produdctDal)
+        {
+            _productDal = produdctDal;
+        }
+
+        public void Save()
+        {
+            _productDal.Save();
+        }
+    }
+}
---
+++ NullObject/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(TestLogger.GetLogger());
+            customerManager.Save();
+        }
+    }
+
+    class CustomerManager
+    {
+        private ILogger _logger;
+
+        public CustomerManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Log();
+            Console.WriteLine("Saved");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+
+    class Log4NetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with Log4Net");
+        }
+    }
+
+    class NLogLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with NLog");
+        }
+    }
+
+    class TestLogger : ILogger
+    {
+        private static TestLogger _testLogger;
+
+        private static object _lock = new object();
+
+        private TestLogger()
+        {
+
+        }
+
+        public static TestLogger GetLogger()
+        {
+            lock (_lock)
+            {
+                if (_testLogger == null)
+                {
+                    _testLogger = new TestLogger();
+                }
+            }
+            return _testLogger;
+        }
+        public void Log()
+        {
+           
+        }
+    }
+
+    class CustomerManagerTest
+    {
+        public void SaveTest()
+        {
+            CustomerManager customerManager = new CustomerManager(TestLogger.GetLogger());
+            customerManager.Save();
+        }
+    }
+}
